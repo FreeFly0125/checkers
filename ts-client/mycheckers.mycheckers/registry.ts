e5383c468a64369ec04408e0bcb930e7c0d479e5
FreeFly0125 <simonbergman516@gmail.com>
2023-08-11 20:13:52 +0200

    fact: implement piece move

--- ts-client/mycheckers.mycheckers/registry.ts
+++ ts-client/mycheckers.mycheckers/registry.ts
@@ -1,7 +1,9 @@
 import { GeneratedType } from "@cosmjs/proto-signing";
+import { MsgPlayMove } from "./types/mycheckers/mycheckers/tx";
 import { MsgCreateGame } from "./types/mycheckers/mycheckers/tx";
 
 const msgTypes: Array<[string, GeneratedType]>  = [
+    ["/mycheckers.mycheckers.MsgPlayMove", MsgPlayMove],
     ["/mycheckers.mycheckers.MsgCreateGame", MsgCreateGame],
     
 ];
